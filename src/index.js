787c26408115f0e818a068a66832d66be1e328a6
Telmuun <tjesusho@gmail.com>
2020-04-22 18:53:02 +0800

    Start TodoApp

--- src/index.js
+++ src/index.js
@@ -1,81 +1,12 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import App from "./Components/App";
+
 import * as serviceWorker from './serviceWorker';
-
-function Row(props) {
-    return (
-        <div>
-            <label>{props.label}</label>
-            {props.children}
-        </div>
-    )
-}
-
-
-function Greeting(props) {
-    const name = useFormInput('Mary');
-    const surname = useFormInput('Clark');
-
-    const width = useWindowResize();
-    useDocumentTitle(name.value + ' ' + surname.value);
-
-
-    return (
-        <div>
-            <Row label="Name">
-                <input {...name}/>
-            </Row>
-            <Row label="Surname">
-                <input {...surname} />
-            </Row>
-
-            <Row label="Width">
-                {width}
-            </Row>
-        </div>
-    )
-}
-
-
-function useFormInput(initialValue) {
-    const [value, setValue] = useState(initialValue);
-
-    function handleChange(e) {
-        setValue(e.target.value)
-    }
-
-    return {
-        value: value,
-        onChange: handleChange
-    }
-}
-
-
-function useDocumentTitle(title) {
-    useEffect(() => {
-        document.title = title
-    });
-}
-
-function useWindowResize() {
-    const [width, setWidth] = useState(window.innerWidth);
-
-    useEffect(() => {
-        const handleResize = () => setWidth(window.innerWidth);
-        window.addEventListener('resize', handleResize);
-
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        }
-    });
-
-    return width
-}
 
 
 ReactDOM.render(
-    <Greeting/>,
+    <App/>,
     document.getElementById('root')
 );
 
