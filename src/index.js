8fe319495df0dab7a8946c26f2759e44448f86ff
Telmuun <tjesusho@gmail.com>
2020-04-27 19:16:42 +0800

    React router setup with couple of pages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 import * as ReactDOM from "react-dom";
-import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 serviceWorker.unregister();
