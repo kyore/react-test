2fcd9289ab2ddd2abe262ed4ae9c6ab28279013a
Telmuun <tjesusho@gmail.com>
2020-04-27 19:06:38 +0800

    Add project setup

--- src/actions/index.js
+++
@@ -1,7 +0,0 @@
-export const increment = (nr = 1) => {
-  return { type: "INCREMENT", payload: nr };
-};
-
-export const decrement = () => {
-  return { type: "DECREMENT" };
-};
--- src/index.js
+++ src/index.js
@@ -1,20 +1,7 @@
 import React from "react";
 import * as ReactDOM from "react-dom";
-import { createStore } from "redux";
-import { Provider } from "react-redux";
 import * as serviceWorker from "./serviceWorker";
-import rootReducers from "./reducers";
-import User from "./components/User";
+import App from "./App";
 
-const store = createStore(
-  rootReducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
-
-ReactDOM.render(
-  <Provider store={store}>
-    <User />
-  </Provider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 serviceWorker.unregister();
